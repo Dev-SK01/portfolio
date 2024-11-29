f4626d0b662c26caf93f1c334f4ff4445a056f63
devsk <devsk@devsk>
2024-11-29 17:42:17 +0530

    portfolio 1.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+
+    return (
+        <div className="footer text-center">
+        <p> 
+          &#x3c;&#47;&#x3e; with ❤️ by
+          <a href="https://dev-sk01.github.io/portfolio" target="_blank">
+            {" "}
+            Srikanth
+          </a>
+          ❤️
+        </p>
+        {/* <p className="pink-text-gradient">No. of Visitors | <img className="visitcounter" src="https://hitwebcounter.com/counter/counter.php?page=9795911&style=0025&nbdigits=5&type=page&initCount=459" title="Counter Widget" Alt="Visit counter For Websites"   border="0" /></p> */}
+
+      </div>
+    );
+  };
+  
+  export default Footer;
---
+++ src/constants/index.js
@@ -0,0 +1,447 @@
+import {
+    javascript,html,css,reactjs,tailwind,nodejs,mongodb,git,
+    hf,school,
+    crypto,oop,yml,
+    port_3d, jsp, react1,
+    be,sql, mini, snl,
+  } from "../assets";
+
+  const profiles = [
+    {
+      link: "https://www.github.com/dev-sk01",
+      icon: "https://img.icons8.com/?size=100&id=efFfwotdkiU5&format=png&color=000000",
+    },
+    {
+      link: "https://www.dly.to/3JxmFPI6R9v",
+      icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEsQpArfo4Mh7CBeXDlZvRpopCp2V0J49Hmkykp7uxLlAfFnojdVZgTPZFSiImp-DMLy8&usqp=CAU",
+    },
+    {
+      link: "https://www.qwiklabs.com/public_profiles/48dcd029-03b4-437b-9dd3-ef7d65958eb0",
+      icon: "https://img.icons8.com/?size=100&id=30998&format=png&color=000000",
+    },
+    {
+      link:"https://dev.to/devsk001",
+      icon: hf,
+    },
+  ];
+
+  const achievements = [
+    {
+      title: "Secured 1st place in 12th: Secured first position in school level(2021).",
+    },
+    {
+      title: "Notable Team : Selected for the notable team in IIT FOSSE Mapathon",
+    },
+    {
+      title: "Google Innovator : Secured  Google Innovator program in 2024",
+    },
+    {
+      title: "1st Position : Zonal level Chess champiom (2019 - 2021).",
+    },
+    {
+      title: "GDG : google Developers Groups member in Chennai,Madurai,Coimbatore",
+    },
+    {
+      title: "Secured 1st place in Athletics (2018).",
+    },
+  ]
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "Mongo DB",
+      icon: mongodb,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name:"Bootstrap",
+      icon: "https://img.icons8.com/color/480/000000/bootstrap.png",
+    },
+    {
+      name:"Typescript",
+      icon: "https://img.icons8.com/?size=100&id=uJM6fQYqDaZK&format=png&color=000000",
+    },
+    {
+      name:"Docker",
+      icon: "https://img.icons8.com/?size=100&id=cdYUlRaag9G9&format=png&color=000000",
+    },
+    {
+      name:"Vitest",
+      icon: "https://img.icons8.com/?size=100&id=dJjTWMogzFzg&format=png&color=000000",
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name:"Redux",
+      icon: "https://img.icons8.com/?size=100&id=jD-fJzVguBmw&format=png&color=000000",
+    },
+    {
+      name:"MySql",
+      icon: "https://img.icons8.com/color/480/000000/mysql-logo.png",
+    },
+    {
+      name:"Nextjs",
+      icon: "https://img.icons8.com/?size=100&id=yUdJlcKanVbh&format=png&color=000000",
+    },
+    {
+      name:"Firebase",
+      icon: "https://img.icons8.com/?size=100&id=87330&format=png&color=000000",
+    },
+    {
+      name:"AWS",
+      icon: "https://img.icons8.com/?size=100&id=33039&format=png&color=000000",
+    },
+    {
+      name:"Google Cloud",
+      icon: "https://img.icons8.com/color/480/000000/google-cloud.png",
+    },
+    
+  ];
+
+  const list = [
+    {
+      id: "web",
+      title: "Web Dev",
+    },
+    {
+      id: "other",
+      title: "Freelancing",
+    },
+    {
+      id: "java",
+      title: "College Project",
+    },
+  ];
+
+  export const javaProject = [
+    {
+      name: "Ucer Idiotz",
+      description:
+        "The project is based Enginering Students Notes and Syllabus base youtube Video Providing platform",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "node",
+          color: "green-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: crypto,
+      source_link: "https://ucer-idiotz.vercel.app/",
+      source_code_link: "https://github.com/dev-sk01",
+    },
+    {
+      name: "Ucer Exam Cell",
+      description:
+        "Designed and Developed autoated exam hall allocation system for college internal use.",
+      tags: [
+        {
+          name: "Expres",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "React",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Node",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: yml,
+      source_link: "https://ucer.web.app/",
+      source_code_link: "https://github.com/dev-sk01/ucer-exam-cell",
+    },
+  ];
+  
+  export const cProject = [ ];
+  
+  export const webProject = [
+    {
+      name: "Notes EDU",
+      description:
+        "Created an impressive Notes making and journalin app for my Own Use",
+      tags: [
+        {
+          name: "React JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Redux",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: port_3d,
+      source_link: "https://dev-sk01.github.io/NotesEDU/",
+      source_code_link: "https://github.com/dev-sk01/NotesEDU",
+    },
+    {
+      name: "Number Kodu",
+      description:
+        "Website made using React in the FrontEnd, Nodejs and Express in the BackEnd. Responsive webpage is made with user friendly interface. It an Fullstack Appliation  information system for local transport ",
+      tags: [
+        {
+          name: "nodejs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "React",
+          color: "green-text-gradient",
+        },
+        {
+          name: "MongoDB",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "ExpressJS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: react1,
+      source_link: "https://dev-sk01.github.io/number-kodu/",
+      source_code_link: "https://github.com/dev-sk01/number-kodu",
+    },
+    {
+      name: "News Da Mapla",
+      description:
+        "Website made using ReactJs and Express in the BackEnd. Its an Realtime News Platform with logIn and signUp functionality. ",
+      tags: [
+        {
+          name: "JavaScript",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "REST API",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jsp,
+      source_link: "https://dev-sk01.github.io/News-Da-Mapla-App/",
+      source_code_link: "https://github.com/dev-sk01/News-Da-Mapla-App",
+    },
+  ];
+  
+  export const otherProject = [
+    {
+      name: "SonthaBoomi",
+      description:
+        "Designed and Developed A Fully Fucntional Website For Real Esate Client",
+      tags: [
+        {
+          name: "html",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: be,
+      source_link: "https://webma-startup.github.io/SonthaBoomi/index.html",
+      source_code_link: "https://github.com/webma-startup/SonthaBoomi",
+    },
+
+    {
+      name: "NXTGEN",
+      description:
+        "Digital Marketing Agency Project . Developed Stunning Website For higher visibility",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Redux",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Vercel",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: sql,
+      source_link: "https://webma-startup.github.io/NXTGEN/index.html",
+      source_code_link: "https://github.com/webma-startup/NXTGEN",
+    },
+    {
+      name: "Devstella",
+      description:
+        "Designed and Developed startup company website. To get into their Digital transforational step.",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Figma",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Node",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: mini,
+      source_link: "https://webma-startup.github.io/Devstella/",
+      source_code_link: "https://github.com/webma-startup/Devstella",
+    },
+    {
+      name: "Kashmir Tourism",
+      description:
+        "This is first freelancing project. We Designed and Developed website their digital presence. ",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Github",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Node",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: snl,
+      source_link: "https://the-kashmir-tourism.vercel.app/",
+      source_code_link: "https://github.com/dev-sk01",
+    },
+    
+  ];
+  
+  const experiences = [
+    {
+      title: "Software Devloper",
+      company_name: "Trio Devs",
+      icon: "https://lh3.googleusercontent.com/p/AF1QipOcMJmSAC07xn4_NxO77lNrO3s0ySID3-pnfh7s=s680-w680-h510",
+      iconBg: "#383E56",
+      date: "Jun 2024 - Aug 2024 ",
+      link: "",
+      points: [
+        "Software build is now used internally in the company and by the company’s clients for operating Netx360 and Netx360+.",
+        "Adapted Agile methodologies, CI/CD, and Test-Driven Programming to uphold strong programming practices.",
+        "Using technologies like Node , React , Javascript",
+      ],
+      link: "https://linktr.ee/webma_startup",
+    },
+    {
+      title: "Fullstack Developer Intern",
+      company_name: "Metal & Weld Farication",
+      icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4LbEOnwI3zZlrMP44Ac0eO1NfDqSRsAQKdQ&s",
+      iconBg: "#383E56",
+      date: "Mar 2022 - Oct 2023",
+      link: "",
+      points: [
+        "Creating a system for metal fabrication automated system",
+        "Collaborating with cross-functional technologies like spring boot, react etc. to integrate high-quality products.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Developed E-commerce site for their products",
+      ],
+      link: "https://linktr.ee/webma_startup",
+    },
+    {
+      title: "FrontEnd Developer Intern",
+      company_name: "Kashiv Info Tech",
+      icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV_xxmpzM33xtiUsI71XSWymSkkXigqHLoVGDljbM4r_63Kz57zU3heHxFARr5e8SiXYU&usqp=CAU",
+      iconBg: "#E6DEDD",
+      date: "May 2024 - Jul 2024",
+      points: [
+        "Worked As FrontEnd Developer in Kashiv Info Tech",
+        "Created A stunning and user friendly website",
+      ],
+      link: "https://linktr.ee/webma_startup",
+    },
+    {
+      title: "Freelancer",
+      company_name: "WebMa",
+      icon: "https://ugc.production.linktr.ee/153376fa-18c1-4665-ad32-3bf90fa7e717_250X250.webp?io=true&size=avatar-v3_0",
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - present",
+      points: [
+        "Worked As Fullstack Developer in WebMa",
+        "Created A stunning and user friendly website",
+        "Developed Realiable and Scalable Systems for Business",
+        "Connected with 10+ Business Oweners",
+      ],
+      link: "https://linktr.ee/webma_startup",
+    },
+    
+  ];
+  
+  const educations = [
+    {
+      degree: "Bachelor of Engineering",
+      branch:
+        "Computer Science & Engineering",
+      marks:
+        "CGPA : 7.5 / 10",
+      name: "Anna University College Of Engineering , Ramanthapuram",
+      year: "(2021 - 2025)",
+      image: "https://logowik.com/content/uploads/images/anna-university3770.jpg",
+    },
+    {
+      degree:
+        "12th Grade",
+      branch : "Science",
+      marks:
+        "Percentage : 85.00 %",
+      name: "Govt Higher Secondary School , Kalaiyarkovil",
+      year: "2021",
+      image: "https://pbs.twimg.com/profile_images/961107678893309957/43VEMGuP_400x400.jpg",
+    },
+    {
+      degree:
+        "10th Grade",
+      branch: "SSC",
+      marks:
+        "Percentage : 86.78 %",
+      name: "Govt Higher Secondary School , Kalaiyarkovil",
+      year: "2019",
+      image: "https://pbs.twimg.com/profile_images/961107678893309957/43VEMGuP_400x400.jpg",
+    },
+  ];
+  
+  export { list, profiles, technologies, experiences, educations, achievements };
